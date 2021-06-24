81f9c2728df33c53a9c3baf31e648f90607d713b
shoshin <shoshinprograms@gmail.com>
2021-06-25 00:46:45 +1000

    Fix typo target and property name. Adjust zig cmake commands

--- CMake/CMakeZIGInformation.cmake
+++ CMake/CMakeZIGInformation.cmake
@@ -1,7 +1,8 @@
 # This file sets the basic flags for the ZIG Compiler
 if(NOT CMAKE_ZIG_COMPILE_OBJECT)
     # -femit-bin here so that zig outputs to the directory that cmake opens to check for object files
-    set(CMAKE_ZIG_COMPILE_OBJECT "<CMAKE_ZIG_COMPILER> build-obj -femit-bin=${PROJECT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}.dir/${PROJECT_NAME}.zig.o <SOURCE>")
+    #set(CMAKE_ZIG_COMPILE_OBJECT "<CMAKE_ZIG_COMPILER> build-obj -femit-bin=${PROJECT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}.dir/${PROJECT_NAME}.zig.o <SOURCE>")
+    set(CMAKE_ZIG_COMPILE_OBJECT "<CMAKE_ZIG_COMPILER> build-lib -femit-bin=${OBJECT} <SOURCE>")
 #
 endif()
 if(NOT CMAKE_ZIG_LINK_EXECUTABLE)

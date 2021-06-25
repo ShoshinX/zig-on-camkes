403b79b549282e708d5fe675ce5c73100d3c43a5
shoshin <shoshinprograms@gmail.com>
2021-06-26 07:05:36 +1000

    Fix cmake include problem

--- components/main/src/run.zig
+++ components/main/src/run.zig
@@ -1,8 +1,10 @@
 const c = @cImport({
     @cInclude("stdio.h");
+    @cInclude("sel4/types.h");
 });
 
 export fn run() i64 {
     _ = c.printf("Hello, world!!\n");
+    var uint: c.seL4_Uint64 = 85;
     return 0;
 }
